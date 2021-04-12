f7d75f01c5595f7de2fd6f55cc12b2a5de1bb276
unknown <damaideo6@gmail.com>
2021-04-12 20:17:26 +0700

    install bottom navigation dan install icon

--- App.js
+++ App.js
@@ -7,14 +7,47 @@
  */
 
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Home from './src/activity/Home';
-
-
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+import styles from './styles.json';
+import {create, getColor} from 'tailwind-rn';
+const {tailwind} = create(styles);
 const App = () => {
   return (
-      <Home/>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'clipboard' : 'clipboard-outline';
+            } else if (route.name === 'Jadwal') {
+              iconName = focused ? 'time' : 'time-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          showLabel: true,
+          activeTintColor: getColor('blue-300'),
+          inactiveTintColor: 'gray',
+          style: tailwind('h-16 py-3'),
+        }}
+        initialRouteName="Home">
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Jadwal" component={Home} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 export default App;
