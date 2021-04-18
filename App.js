47375c0e46624d4dfc662a8c8940437ffae168a1
unknown <damaideo6@gmail.com>
2021-04-18 13:58:55 +0700

    mengubah logo dan menambahkan splashscreen

--- App.js
+++ App.js
@@ -10,8 +10,10 @@ import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import {Text, Alert, BackHandler, PermissionsAndroid} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import styles from './styles.json';
 import {create, getColor} from 'tailwind-rn';
+import SplashScreen from 'react-native-splash-screen';
+
+import styles from './styles.json';
 import Navigation from './Navigation';
 const {tailwind} = create(styles);
 Text.defaultProps = Text.defaultProps || {};
@@ -48,7 +50,9 @@ const App = () => {
     } catch (err) {
       console.warn(err);
     }
-  });
+
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <Navigation />
